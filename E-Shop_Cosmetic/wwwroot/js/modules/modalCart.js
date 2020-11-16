2ae7f601e5f822c0b4a4f9f5458e1c861148de69
иван кузьмич <ivankuzmic210@gmail.com>
2020-11-16 16:53:44 +0300

    add slider and modules

---
+++ E-Shop_Cosmetic/wwwroot/js/modules/modalCart.js
@@ -0,0 +1,15 @@
+﻿export function modalCart() {
+    function eventModalCart() {
+        modalCart.classList.toggle("on-click");
+    }
+    const cardBtn = document.querySelector("#btn-basket");
+    const modalCart = document.querySelector(".modal-cart");
+    const close = document.querySelector(".btn-close");
+    const cancel = document.querySelector(".btn-cancel");
+
+    cardBtn.addEventListener("click", eventModalCart);
+
+    close.addEventListener("click", eventModalCart);
+
+    cancel.addEventListener("click", eventModalCart);
+}
---
+++ E-Shop_Cosmetic/wwwroot/js/modules/modalFilter.js
@@ -0,0 +1,16 @@
+﻿// modal filter
+export function modalFilter() {
+    function eventModalFilter() {
+        modalSearch.classList.toggle("on-click");
+    }
+    const filterBtn = document.querySelector('#btn-filter');
+    const modalSearch = document.querySelector(".modal-search");
+    const closeFilter = document.querySelector(".btn-close-filter");
+    const cancelFilter = document.querySelector(".btn-cancel-filter");
+
+    filterBtn.addEventListener("click", eventModalFilter);
+
+    closeFilter.addEventListener("click", eventModalFilter);
+
+    cancelFilter.addEventListener("click", eventModalFilter);
+}
