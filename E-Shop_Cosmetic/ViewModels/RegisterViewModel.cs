8601d1d7ac63a93298825927ec6a2677ca6350aa
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2020-12-01 12:38:56 +0300

    Register edited

--- E-Shop_Cosmetic/ViewModels/RegisterViewModel.cs
+++ E-Shop_Cosmetic/ViewModels/RegisterViewModel.cs
@@ -9,6 +9,7 @@ namespace E_Shop_Cosmetic.ViewModels
     public class RegisterViewModel
     {
         [Required(ErrorMessage = "Не указан Email")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Не указан пароль")]
