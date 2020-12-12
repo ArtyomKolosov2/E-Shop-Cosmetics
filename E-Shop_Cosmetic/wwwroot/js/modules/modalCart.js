494874ff17d06eec795a2b6bf7afc6372fa9467f
иван кузьмич <ivankuzmic210@gmail.com>
2020-12-13 00:45:16 +0300

    fix form addProduct, DeleteProduct, UpdateProduct fix orderFilter, ProductFilter js refactor modalCart, modalFilter edit admin password

--- E-Shop_Cosmetic/wwwroot/js/modules/modalCart.js
+++ E-Shop_Cosmetic/wwwroot/js/modules/modalCart.js
@@ -5,11 +5,8 @@
     const cardBtn = document.querySelector("#btn-basket");
     const modalCart = document.querySelector(".modal-cart");
     const close = document.querySelector(".btn-close");
-    const cancel = document.querySelector(".btn-cancel");
 
     cardBtn.addEventListener("click", eventModalCart);
 
     close.addEventListener("click", eventModalCart);
-
-    cancel.addEventListener("click", eventModalCart);
 }
