b54b37e65d3fecb10d293fb8b7eb122e0d47e03e
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2020-12-13 00:47:48 +0300

    minor fixes

---
+++ E-Shop_Cosmetic/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using E_Shop_Cosmetic.Data.Interfaces;
+using E_Shop_Cosmetic.Data.Models;
+using E_Shop_Cosmetic.Data.Specifications.Base;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace E_Shop_Cosmetic.Data.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoriesRepository
+    {
+        public CategoryRepository(AppDBContext appDBContext) : base(appDBContext)
+        {
+        }
+
+        public async Task AddCategoryAsync(Category category)
+        {
+            await AddAsync(category);
+        }
+
+        public async Task DeleteCategoryAsync(Category category)
+        {
+            await DeleteAsync(category);
+        }
+
+        public async Task<IReadOnlyList<Category>> GetCategoriesAsync()
+        {
+            return await GetAllAsync();
+        }
+
+        public async Task<IReadOnlyList<Category>> GetCategoriesAsync(ISpecification<Category> specification)
+        {
+            return await GetAllAsync(specification);
+        }
+
+        public async Task<Category> GetCategoryByIdAsync(int id)
+        {
+            return await GetByIdAsync(id);
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            await UpdateAsync(category);
+        }
+    }
+}
