716ea7e03e39cd1b99cef7a93e957e53851d1273
иван кузьмич <ivankuzmic210@gmail.com>
2020-12-12 18:08:58 +0300

    fix crop text index page, showProduct

---
+++ E-Shop_Cosmetic/wwwroot/js/modules/cropText.js
@@ -0,0 +1,21 @@
+﻿export function cropTextArray(textArray, size, endCharacter) {
+    textArray.forEach(el => {
+        let text = el.innerHTML;
+
+        if (el.innerHTML.length > size) {
+            text = text.substr(0, size);
+            el.innerHTML = text + endCharacter;
+        }
+    });
+
+};
+
+export function cropText(text, size, endCharacter) {
+    let data = text.innerHTML;
+
+    if (data.length > size) {
+        data = data.substr(0, size);
+        text.innerHTML = data + endCharacter;
+    }
+
+};
