d873341bce6f434ce79d43537e8e9b9f7d48c663
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2021-03-08 22:39:37 +0300

    Product repository

--- E-Shop_Cosmetic/Data/interfaces/IProductsRepository.cs
+++ E-Shop_Cosmetic/Data/interfaces/IProductsRepository.cs
@@ -5,16 +5,10 @@ using System.Threading.Tasks;
 
 namespace E_Shop_Cosmetic.Data.Interfaces
 {
-    public interface IProductsRepository
+    public interface IProductsRepository : IRepository<Product>
     {
-        Task<IReadOnlyList<Product>> GetProductsAsync();
-        Task<IReadOnlyList<Product>> GetProductsAsync(ISpecification<Product> specification);
-        Task<Product> GetProductByIdAsync(int productId);
-        Task<IReadOnlyList<Product>> GetProductsByIdsAsync(IEnumerable<int> ids);
-        Task<Product> GetProductByNameAsync(string name);
-        Task<Product> GetProductByPriceAsync(int price);
-        Task AddProductAsync(Product product);
-        Task DeleteProductAsync(Product product);
-        Task UpdateProductAsync(Product product);
+        Task<IReadOnlyList<Product>> GetProductsByIds(IEnumerable<int> ids);
+        Task<Product> GetProductByName(string name);
+        Task<Product> GetProductByPrice(int price);
     }
 }
